0d1fba7838a2f01ce7fd065cae8bc74a32b0a4ff
Ricardo <rick-queiroz@live.com>
2024-04-17 21:39:44 -0300

    Integração da API de Pokemons e adição dos Pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,105 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app class="app-container">
+    <v-container >
+      <v-row >
+        <v-col
+          cols="2" 
+          v-for="pokemon in filtered_pokemons"
+          :key="pokemon.name"
+        >
+          <v-card>
+            <v-container >
+              <v-row class="mx-0 d-flex justify-center">
+                <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(pokemon)}.png`" :alt="pokemon.name" width="80%">
+                <h2 class="text-center">{{get_name(pokemon)}}</h2>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+
+import axios from 'axios';
+export default {
+  name: "App",
+
+  components: {},
+
+  data(){
+    return {
+      pokemons: [],
+      search:"",
+      filterBy: "name",
+      filterOptions: ["name", "type", "id", "species"]
+    }
+  },
+
+  mounted(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151").then((response) => {
+      this.pokemons = response.data.results;
+    })
+  },
+  methods: {
+    get_id(pokemon){
+      return pokemon.url.split("/")[6];
+    },
+    get_name(pokemon){
+      return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    }
+  },
+  computed: {
+    filtered_pokemons() {
+      return this.pokemons.filter((item) => {
+        switch (this.filterBy) {
+          case "name":
+            return item.name.includes(this.search);
+          case "type":
+            return true; 
+          case "id":
+          if (this.search === "") {
+            return true; 
+          } else if (!isNaN(this.search)) {
+            return parseInt(this.get_id(item)) === parseInt(this.search);
+          }
+          case "species":
+            return true; 
+          default:
+            return true;
+        }
+      });
+    }
+  }
+};
+
+
+
+</script>
+
+<style>
+.app-container {
+  background: linear-gradient(
+      to bottom right,
+      rgb(255, 0, 0),
+      rgb(255, 255, 255)
+    )
+    no-repeat center center fixed !important;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
+  color: rgb(0, 0, 0);
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+
+.custom-text-field,
+.custom-select {
+  color: #333; 
+  background-color: #fff; 
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
 import './style.css'
+import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/dist/vuetify.min.css'
+
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
